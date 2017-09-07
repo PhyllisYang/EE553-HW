5b82cfb533bde620caffae371c53a11aafbdcde6
PhyllisYang <phyllisyang1103@gmail.com>
2017-09-07 18:49:12 -0400

    add some comment in the hw1f

--- HW1/hw1f.cpp
+++ HW1/hw1f.cpp
@@ -3,6 +3,7 @@
 */
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -11,19 +12,18 @@ int main()
   cout << "Please enter a number:\n";
   cin >> m;
   for ( n=1; n<=m; n++)
-    {
       s1 += 1/(n*n);
-    }
-  cout << s1 <<'\n';
+  cout << setprecision(7) <<s1 <<'\n';
   cout << "Please enter another number:\n";
   cin >> l;
   for (n=1; n<=l; n++)
     s2 += 1/(n*n);
-  cout << s2 <<'\n';
+  cout << setprecision(7) << s2 <<'\n';
 
-  cout << sqrt(6*s1) << '\n';
-  cout << sqrt(6*s2) << '\n';
+  cout << setprecision(7) << sqrt(6*s1) << '\n';
+  cout << setprecision(7) << sqrt(6*s2) << '\n';
   
   return 0;
 
 }
+/* iterations needed before the contribution of 1/n^2 is too small and no longer matters is 4090 */
