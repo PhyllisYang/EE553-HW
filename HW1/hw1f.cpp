5ec82d2de39c3b42a7d66f09b0a9639ac0321929
PhyllisYang <phyllisyang1103@gmail.com>
2017-09-07 10:31:20 -0400

    add hw1f.cpp

---
+++ HW1/hw1f.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  float s1 = 0, s2=0, n, m, l;
+  cout << "Please enter a number:\n";
+  cin >> m;
+  for ( n=1; n<=m; n++)
+    {
+      s1 += 1/(n*n);
+    }
+  cout << s1 <<'\n';
+  cout << "Please enter another number:\n";
+  cin >> l;
+  for (n=1; n<=l; n++)
+    s2 += 1/(n*n);
+  cout << s2 <<'\n';
+
+  cout << sqrt(6*s1) << '\n';
+  cout << sqrt(6*s2) << '\n';
+  
+  return 0;
+
+}
